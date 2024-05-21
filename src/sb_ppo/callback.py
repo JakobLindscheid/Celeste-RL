d0a4a4dad8097ef148ceecee577f7ecc979d5d63
danielvhvs <danielvhvs@gmail.com>
2024-05-21 12:26:41 +0200

    callback fixed and added gitignore

--- src/sb_ppo/callback.py
+++ src/sb_ppo/callback.py
@@ -12,13 +12,16 @@ class CheckpointCallback(BaseCallback):
     ):
         super().__init__(verbose)
         self.save_freq = save_freq
-        self.save_path = os.path.join(save_path, "rl_model/")
-        # self.save_path =save_path
-
-    def _init_callback(self) -> None:
-        # Create folder if needed
+        self.save_path = save_path
         if self.save_path is not None:
             os.makedirs(self.save_path, exist_ok=True)
+
+        self.save_path = os.path.join(self.save_path, "rl_model.zip")
+
+    # def _init_callback(self) -> None:
+    #     # Create folder if needed
+    #     if self.save_path is not None:
+    #         os.makedirs(self.save_path, exist_ok=True)
 
     def _on_step(self) -> bool:
         if self.n_calls % self.save_freq == 0:
